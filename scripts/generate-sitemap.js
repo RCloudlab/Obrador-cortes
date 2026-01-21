589773a7f2344a58e5716ecdbd3d8f3dd0e58ec3
RCloudLab <rvesp04@hotmail.com>
2026-01-21 13:13:11 -0600

    Fix sitemap errors

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -3,20 +3,22 @@ import { createWriteStream } from 'fs';
 
 const links = [
   { url: '/', changefreq: 'daily', priority: 1.0 },
-  { url: '/mayoreo', changefreq: 'monthly', priority: 0.7 },
+  { url: '/carnes', changefreq: 'monthly', priority: 0.7 },
   { url: '/carnitas', changefreq: 'weekly', priority: 0.8 },
   { url: '/contacto', changefreq: 'monthly', priority: 0.7 },
 ];
 
-const sitemap = new SitemapStream({ hostname: 'https://carnitascortes.com' });
+// Tu dominio
+const sitemap = new SitemapStream({ hostname: 'https://www.carnitascortes.com' });
 
 const writeStream = createWriteStream('./public/sitemap.xml');
 sitemap.pipe(writeStream);
-streamToPromise(
-  links.reduce((acc, item) => acc.write(item), sitemap)
-)
-  .then(() => {
-    sitemap.end();
-    console.log('Sitemap generado en /public/sitemap.xml');
-  })
-  .catch(console.error);
+
+// Escribimos los links uno por uno
+links.forEach(link => sitemap.write(link));
+
+sitemap.end();
+
+streamToPromise(sitemap).then(() => {
+  console.log('Sitemap generado exitosamente en /public/sitemap.xml');
+}).catch(console.error);
